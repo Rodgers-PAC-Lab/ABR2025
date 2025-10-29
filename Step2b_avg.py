7230310cc631c454be4881f4d8e84b2286bbfe7d
Chris Rodgers <xrodgers@gmail.com>
2025-10-28 23:07:38 -0400

    update

--- Step2b_avg.py
+++ Step2b_avg.py
@@ -15,10 +15,18 @@ import json
 import scipy.signal
 import numpy as np
 import pandas
+import paclab
+import paclab.abr.abr_plotting
 from paclab import abr
 import my.plot
 import matplotlib.pyplot as plt
 import tqdm
+
+
+## Plotting defaults
+my.plot.manuscript_defaults()
+my.plot.font_embed()
+MU = chr(956)
 
 
 ## Paths
@@ -36,9 +44,6 @@ output_directory = paths['output_directory']
 abs_max_sigma = 3
 stdev_sigma = 3
 
-# Loudest dB
-loudest_db = 93
-
 
 ## Load previous results
 # Load results of Step1
@@ -52,6 +57,9 @@ big_triggered_neural = pandas.read_pickle(
     os.path.join(output_directory, 'big_triggered_neural'))
 big_click_params = pandas.read_pickle(
     os.path.join(output_directory, 'big_click_params'))
+
+# Loudest dB
+loudest_db = big_triggered_neural.index.get_level_values('label').max()
 
 
 ## Join on speaker_side
@@ -131,14 +139,14 @@ if PLOT_SINGLE_TRIAL_ABR:
     # Slice out loudest only
     single_trial_abr = single_trial_abr.xs(loudest_db, level='label')
 
-    f, ax = plt.subplots(figsize=(4, 2.5))
-    f.subplots_adjust(bottom=.24, left=.15, right=.95, top=.89)
+    f, ax = plt.subplots(figsize=(3.75, 2.5))
+    f.subplots_adjust(bottom=.24, left=.18, right=.95, top=.89)
     ax.plot(
         single_trial_abr.columns.values / 16e3 * 1000,
         single_trial_abr.T * 1e6, 
         color='k', alpha=.05, lw=1)
     ax.set_xlabel('time from sound (ms)')
-    ax.set_ylabel('ABR (uV)')
+    ax.set_ylabel(f'ABR ({MU}V)')
     ax.set_yticks((-8, 0, 8))
     ax.set_ylim((-8, 8))
     ax.set_xlim((-2.5, 7.5))
@@ -152,72 +160,103 @@ if PLOT_SINGLE_TRIAL_ABR:
     f.savefig('figures/PLOT_SINGLE_TRIAL_ABR.png', dpi=300)
 
 if PLOT_TRIAL_AVERAGED_ABR:
-    # Plot the corresponding trial averaged ABR
-    trial_averaged_abr = big_triggered_neural2.loc[
-        datetime.date(2025, 5, 20)].loc['Cat_229'].loc[10].xs(
-        'RV', level='channel').droplevel('speaker_side')
+    ## Plot the ABR averaged by level
+    
+    # Slice out Cat_229 rec 10 on 2025-05-20, RV for L speaker
+    to_agg = big_triggered_neural2.loc[
+        datetime.date(2025, 5, 20)].loc[
+        'Cat_229'].loc[
+        10].loc[
+        'RV'].loc[
+        'L']
 
     # Aggregate
-    trial_averaged_abr = trial_averaged_abr.groupby('label').mean()
+    trial_averaged_abr = to_agg.groupby('label').mean()
     
+    # Set up colorbar
+    # Always do the lowest labels last
+    label_l = sorted(trial_averaged_abr.index, reverse=True)
+    aut_colorbar = paclab.abr.abr_plotting.generate_colorbar(
+        len(label_l), mapname='inferno_r', start=0.15, stop=1)[::-1]
+
     # Plot
-    f, ax = plt.subplots(figsize=(4, 2.5))
-    f.subplots_adjust(bottom=.24, left=.15, right=.95, top=.89)
-    ax.plot(
-        trial_averaged_abr.columns.values / 16e3 * 1000,
-        trial_averaged_abr.T * 1e6, 
-        color='k', lw=1)
+    f, ax = plt.subplots(figsize=(3.75, 2.5))
+    f.subplots_adjust(bottom=.24, left=.18, right=.95, top=.89)
+    for label in label_l:
+        xtopl = trial_averaged_abr.columns.values / 16e3 * 1000
+        ytopl = trial_averaged_abr.loc[label] * 1e6
+        color = aut_colorbar[label_l.index(label)]       
+        ax.plot(xtopl, ytopl, color=color, lw=1)
+    
+    # Legend
+    for n_label, (label, color) in enumerate(zip(label_l, aut_colorbar)):
+        if np.mod(n_label, 2) != 0:
+            continue
+        f.text(
+            1.02, .75 - n_label * .035, f'{label} dB',
+            color=color, ha='center', va='center', size=12)
+
+    # Pretty
     ax.set_xlabel('time from sound (ms)')
-    ax.set_ylabel('ABR (uV)')
-    ax.set_yticks((-6, 0, 6))
-    ax.set_ylim((-6, 6))
+    ax.set_ylabel(f'ABR ({MU}V)')
+    ax.set_yticks((-4, 0, 4))
+    ax.set_ylim((-4, 4))
     ax.set_xlim((-2.5, 7.5))
     ax.set_xticks((-2, 0, 2, 4, 6, ))
     my.plot.despine(ax)
     
     # Plot the stimulus
-    ax.plot([0, 0.1], [5.5, 5.5], 'k-', lw=3)
+    ax.plot([0, 0.1], [3.5, 3.5], 'k-', lw=3)
     
     # Plot the baseline period
-    ax.fill_between([-2.5, -1.25], y1=-6, y2=6, color='orange', alpha=0.5)
+    #~ ax.fill_between([-2.5, -1.25], y1=-6, y2=6, color='orange', alpha=0.5)
     
     f.savefig('figures/PLOT_TRIAL_AVERAGED_ABR.svg')
     f.savefig('figures/PLOT_TRIAL_AVERAGED_ABR.png', dpi=300)
 
 if PLOT_POSITIVE_AND_NEGATIVE_CLICKS:
-    # Plot the corresponding trial averaged ABR
-    trial_averaged_abr = big_triggered_neural2.loc[
-        datetime.date(2025, 5, 20)].loc['Cat_229'].loc[10].xs(
-        'RV', level='channel').droplevel('speaker_side')
+    ## Plot response to positive and negative clicks for a single recording
+    # Only Pineapple_197 recs 6 and 7 shows speaker artefact, and even 
+    # that one is not so bad
+    
+    # Slice out Cat_229 rec 10 on 2025-05-20, RV for L speaker
+    to_agg = big_triggered_neural2.loc[
+        datetime.date(2025, 5, 20)].loc[
+        'Cat_229'].loc[
+        10].loc[
+        'RV'].loc[
+        'L']
 
     # Slice out loudest only
-    trial_averaged_abr = trial_averaged_abr.xs(loudest_db, level='label')
+    to_agg = to_agg.xs(loudest_db, level='label')
     
     # Aggregate
-    trial_averaged_abr = trial_averaged_abr.groupby('polarity').mean()
+    trial_averaged_abr = to_agg.groupby('polarity').mean()
     
     # Plot
-    f, ax = plt.subplots(figsize=(4, 2.5))
-    f.subplots_adjust(bottom=.24, left=.15, right=.95, top=.89)
+    f, ax = plt.subplots(figsize=(3.75, 2.5))
+    f.subplots_adjust(bottom=.24, left=.18, right=.95, top=.89)
     ax.plot(
         trial_averaged_abr.columns.values / 16e3 * 1000,
         trial_averaged_abr.loc[True] * 1e6, 
-        color='k', lw=1)
+        color='k', lw=1, label='positive')
     ax.plot(
         trial_averaged_abr.columns.values / 16e3 * 1000,
         trial_averaged_abr.loc[False] * 1e6, 
-        color='k', lw=1, ls='--')
+        color='k', lw=1, ls='--', label='negative')
     ax.set_xlabel('time from sound (ms)')
-    ax.set_ylabel('ABR (uV)')
-    ax.set_yticks((-6, 0, 6))
-    ax.set_ylim((-6, 6))
+    ax.set_ylabel(f'ABR ({MU}V)')
+    ax.set_yticks((-4, 0, 4))
+    ax.set_ylim((-4, 4))
     ax.set_xlim((-2.5, 7.5))
     ax.set_xticks((-2, 0, 2, 4, 6, ))
     my.plot.despine(ax)
     
     # Plot the stimulus
-    ax.plot([0, 0.1], [5.5, 5.5], 'k-', lw=3)
+    ax.plot([0, 0.1], [3.5, 3.5], 'k-', lw=3)
     
+    # Legend
+    ax.legend(frameon=False, fontsize=12, loc='upper right', bbox_to_anchor=(1.1, 1.1))
     
     f.savefig('figures/PLOT_POSITIVE_AND_NEGATIVE_CLICKS.svg')
     f.savefig('figures/PLOT_POSITIVE_AND_NEGATIVE_CLICKS.png', dpi=300)
